df15600bc7442b9ee044c3821d25127035f455e0
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-12-10 15:16:58 +0100

    Figuring out the best way to create the drag n drop grid

--- src/components/GridLine.tsx
+++ src/components/GridLine.tsx
@@ -1,17 +1,17 @@
 import "../styles/inventoryGrid.css";
-function GridLine({ number }: { number: number }) {
+function GridLine({ number, value }: { number: number; value: any }) {
   return (
     <div className={`gridLine ${number}`}>
-      <div className="gridItem"></div>
-      <div className="gridItem"></div>
-      <div className="gridItem"></div>
-      <div className="gridItem"></div>
-      <div className="gridItem"></div>
-      <div className="gridItem"></div>
-      <div className="gridItem"></div>
-      <div className="gridItem"></div>
-      <div className="gridItem"></div>
-      <div className="gridItem"></div>
+      <div className={`gridItem 0 ${number}`}></div>
+      <div className={`gridItem 1 ${number}`}></div>
+      <div className={`gridItem 2 ${number}`}></div>
+      <div className={`gridItem 3 ${number}`}></div>
+      <div className={`gridItem 4 ${number}`}></div>
+      <div className={`gridItem 5 ${number}`}></div>
+      <div className={`gridItem 6 ${number}`}></div>
+      <div className={`gridItem 7 ${number}`}></div>
+      <div className={`gridItem 8 ${number}`}></div>
+      <div className={`gridItem 9 ${number}`}></div>
     </div>
   );
 }
