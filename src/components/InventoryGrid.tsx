3e7cf114fc4a3770114ae964b7ee643366cb44a5
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-12-10 14:31:28 +0100

    Setting up inventory grid

--- src/components/InventoryGrid.tsx
+++ src/components/InventoryGrid.tsx
@@ -1,5 +1,11 @@
+import "../styles/inventoryGrid.css";
+
 function InventoryGrid() {
-  return <div className="gridHolder"></div>;
+  return (
+    <div className="gridHolder">
+      <div className="gridBox"></div>
+    </div>
+  );
 }
 
 export default InventoryGrid;
