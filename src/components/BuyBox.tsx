af4d5b8f4d03402b5c914f8c49276bdeda5c5b94
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-12-16 09:02:03 +0100

    Styling purchase button

--- src/components/BuyBox.tsx
+++ src/components/BuyBox.tsx
@@ -1,20 +1,58 @@
-import { Dispatch, SetStateAction, useCallback, useContext } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  SyntheticEvent,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
 import { ITraderItem } from "../interfaces";
 import { GlobalContext } from "../App";
 
 function BuyBox({ item }: { item: ITraderItem }) {
   const globalContext = useContext(GlobalContext);
+
+  const [purchaseQuant, setPurchaseQuant] = useState<number>(1);
   const handleCloseClick = () => {
     globalContext.setBuyBox(false);
+    setPurchaseQuant(1);
+  };
+
+  const handleInputChange = (e: SyntheticEvent) => {
+    const target = e.target as HTMLInputElement;
+
+    if (target) {
+      setPurchaseQuant(+target.value);
+    }
   };
   return (
     <div className={globalContext.buyBox ? "buyBox open" : "buyBox hidden"}>
-      <p className="bbHeader">Purchase {item.name}</p>
-      <input className="bbInput" type="number"></input>
-      <button className="bbButton">Confirm</button>
-      <button className="bbButton" onClick={handleCloseClick}>
-        Close
-      </button>
+      <div className="bbHeader">Purchase</div>
+      <div className="bbItemName">{item.name}</div>
+      <div className="bbPrice">
+        {globalContext.traderCurency}
+        {item.price * purchaseQuant}
+      </div>
+      <div className="bbInputHolder">
+        <label htmlFor="bbInput" className="bbLabel">
+          Quantity
+        </label>
+        <input
+          className="bbInput"
+          type="number"
+          onChange={(e) => handleInputChange(e)}
+          value={purchaseQuant}
+        ></input>
+      </div>
+
+      <div className="bbButonHolder">
+        <button className="bbButton" onClick={handleCloseClick}>
+          Confirm
+        </button>
+        <button className="bbButton" onClick={handleCloseClick}>
+          Close
+        </button>
+      </div>
     </div>
   );
 }
